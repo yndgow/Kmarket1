0fc2ab85854a231979d9731a5e994a8a35451cee
bskim617 <bskim617@gmail.com>
2022-12-08 12:53:03 +0900

    2022/12/08 VO

--- Kmarket_JSP/Kmarket/src/main/java/kr/co/kmarket/vo/MemberPointVO.java
+++ Kmarket_JSP/Kmarket/src/main/java/kr/co/kmarket/vo/MemberPointVO.java
@@ -1,5 +1,42 @@
 package kr.co.kmarket.vo;
 
 public class MemberPointVO {
+	private int pointNo;
+	private String uid;
+	private int ordNo;
+	private int point;
+	private String pointDate;
+	
+	public int getPointNo() {
+		return pointNo;
+	}
+	public void setPointNo(int pointNo) {
+		this.pointNo = pointNo;
+	}
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public int getOrdNo() {
+		return ordNo;
+	}
+	public void setOrdNo(int ordNo) {
+		this.ordNo = ordNo;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getPointDate() {
+		return pointDate;
+	}
+	public void setPointDate(String pointDate) {
+		this.pointDate = pointDate;
+	}
+	
 
 }
